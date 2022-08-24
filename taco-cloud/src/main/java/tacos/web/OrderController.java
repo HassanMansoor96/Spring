dd334864700d1809b5d1c46385687a819d93b2c6
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-08-24 09:43:33 +0200

    chapter3.11

--- taco-cloud/src/main/java/tacos/web/OrderController.java
+++ taco-cloud/src/main/java/tacos/web/OrderController.java
@@ -9,28 +9,36 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
-import lombok.extern.slf4j.Slf4j;
-import tacos.TacoOrder;
 
-@Slf4j
+import tacos.TacoOrder;
+import tacos.data.OrderRepository;
+
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
 public class OrderController {
+
+	private OrderRepository orderRepo;
+
+	public OrderController(OrderRepository orderRepo) {
+		this.orderRepo = orderRepo;
+	}
+
 	@GetMapping("/current")
 	public String orderForm() {
 		return "orderForm";
 	}
 
 	@PostMapping
-	public String processOrder(@Valid TacoOrder order, Errors errors, 
-	 SessionStatus sessionStatus) {
-	 if (errors.hasErrors()) {
-	 return "orderForm";
-	 }
-	 log.info("Order submitted: {}", order);
-	 sessionStatus.setComplete();
-	 
-	 return "redirect:/";
+	public String processOrder(@Valid TacoOrder order, Errors errors, SessionStatus sessionStatus) {
+		if (errors.hasErrors()) {
+			return "orderForm";
+		}
+
+		orderRepo.save(order);
+		sessionStatus.setComplete();
+
+		return "redirect:/";
 	}
+
 }
